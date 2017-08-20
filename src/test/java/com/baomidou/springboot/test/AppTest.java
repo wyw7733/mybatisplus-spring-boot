a9020fdd2361af5f836f1dc3ded1572b175b25e0
wyw7733 <wyw890703@163.com>
2017-08-20 10:56:07 +0800

    测试用例

--- src/test/java/com/baomidou/springboot/test/AppTest.java
+++ src/test/java/com/baomidou/springboot/test/AppTest.java
@@ -1,3 +1,20 @@
 package com.baomidou.springboot.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class AppTest {
+	
+	@Autowired
+    private com.baomidou.springboot.service.MailService MailService;
+
+    /*@Test
+    public void testSimpleMail() throws Exception {
+        MailService.sendSimpleMail("ityouknow@126.com","test simple mail"," hello this is simple mail");
+    }*/
 }
